30c05c916d8d5fc08b8c2d4e1c7e759fd4152382
MAlexxiT <mauricio230600@gmail.com>
2023-02-19 23:51:42 -0700

    Se agregaron funciones para calcular combinaciones y permutaciones modulares y no modulares

---
+++ Math/modBinCoef.cpp
@@ -0,0 +1,12 @@
+long long int inf = 10000000007;
+class binCoef{
+    long long int lim;
+    long long int* fact;
+public:
+    binCoef(long long int l){ 
+        lim = l; fact = new long long int[l+1];fact[0]= 1;
+        for(long long int i = 1; i<=l; i++) fact[i] = (fact[i-1]*i)%inf;    
+    }
+    //perm = (fact[n] * modinverse(fac[n-k],inf)%inf;
+    long long int query(long long int n, long long int k){ return fact[n] * modinverse((fac[n-k]*fact[k])%inf,inf)%inf;} 
+};
