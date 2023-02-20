de6f3c167d2cd5b42e95208ed1e715ac0d506281
MAlexxiT <mauricio230600@gmail.com>
2023-02-20 00:04:20 -0700

    Se corrigio el hecho de que no consideraba n<k = 0

--- Math/binCoef.cpp
+++ Math/binCoef.cpp
@@ -1,5 +1,6 @@
 //Solo usar con n<=20
 unsigned long long int bincoef(unsigned long long int n, unsigned long long int k){
+    if(n<k) return 0;
     unsigned long long int num  = 1, den= 1;
     for(unsigned long long int i = (n-k)+1; i<=n; i++) num*=i;
     for(unsigned long long int i = 2; i<=k; i++) den*=i;
--- Math/modBinCoef.cpp
+++ Math/modBinCoef.cpp
@@ -8,5 +8,8 @@ public:
         for(long long int i = 1; i<=l; i++) fact[i] = (fact[i-1]*i)%inf;    
     }
     //perm = (fact[n] * modinverse(fac[n-k],inf)%inf;
-    long long int query(long long int n, long long int k){ return fact[n] * modinverse((fac[n-k]*fact[k])%inf,inf)%inf;} 
+    long long int query(long long int n, long long int k){ 
+        if(n<k) return 0;
+        return (fact[n] * modinverse((fac[n-k]*fact[k])%inf,inf))%inf;
+    } 
 };
