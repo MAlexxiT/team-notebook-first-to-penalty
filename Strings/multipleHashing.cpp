c93e9e1e4b937267ed045868368db0f2b37e66ae
MAlexxiT <mauricio230600@gmail.com>
2023-03-02 12:03:58 -0700

    Se corrigio bug que provocaba colisiones en el hashing

--- Strings/multipleHashing.cpp
+++ Strings/multipleHashing.cpp
@@ -1,49 +1,41 @@
+#include "bits/stdc++.h"
+using namespace std;
 struct multhash{
-    unsigned long long int h1,h2,h3;
+    unsigned long long int h1,h2;
     unsigned long long int alf[257];
-    bool operator < (multhash b) const { // override less than operator
-		if (h1 != b.h1) return h1 < other.h1;
-		if (h2 != b.h2) return h2 < other.h2;
-		return h3 < b.h3;
+    bool operator < (multhash b) const { 
+		if (h1 != b.h1) return h1 < b.h1;
+		return h2 < b.h2;
 	}
-	bool operator == (multhash b) const { // override equal operator
-		return (h1== b.h1 && h2== b.h2 && h3== b.h3)
-	}
+	bool operator == (multhash b) const { return (h1== b.h1 && h2== b.h2);}
+	bool operator != (multhash b) const { return !(h1== b.h1 && h2== b.h2);}
 public: 
     string s;
     multhash(){
-        h1 = 0; h2 = 0;h3 = 0; s = "";
+        h1 = 0; h2 = 0;s = "";
         for(char l = 'a'; l<='z'; l++) alf[l] = l-'a'+1;
     }
     void innit(){
         unsigned long long int inf,p,op;
         
-        inf = 666666555557777777;
-        p = 47;op = 47;
+        inf = 999727999;
+        p = 325255434;op = 325255434;
         for(char l: s){
             h1+=(p*alf[l])%inf;
             p*=op;
             p%=inf;
         }
 
-        inf = 986143414027351997;
-        p = 53;op = 53;
+        inf = 1070777777;
+        p = 10018302;op = 10018302;
         for(char l: s){
             h2+=(p*alf[l])%inf;
             p*=op;
             p%=inf;
         }
-
-        inf = 909090909090909091;
-        p = 79;op = 79;
-        for(char l: s){
-            h3+=(p*alf[l])%inf;
-            p*=op;
-            p%=inf;
-        }
     }
 };
-//VALORES POSIBLES DE INF, MIENTRAS MAS CERCANOS A 10^17 MEJOR
+//VALORES ALTERNATIVOS DE INF, LOG 17
 //666666555557777777
 //986143414027351997
 //974383618913296759
@@ -52,3 +44,6 @@ public:
 //909090909090909091
 //VALORES PARA P, USAR PRIMOS MAYORES A |Alfabeto|
 //31,47,53,61,79
+int main(){
+    return 0;
+}
