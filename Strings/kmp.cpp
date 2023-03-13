14847b61e7b0f236656edae602a01ed88ed81567
MAlexxiT <mauricio230600@gmail.com>
2023-03-12 21:44:23 -0600

    Se arreglo bug que ciclaba el preprocesamiento para ciertas strings

--- Strings/kmp.cpp
+++ Strings/kmp.cpp
@@ -2,7 +2,7 @@ string T,P;
 int bt[MAXN];
 //O(|Text|+|Pattern|)
 void KMPpre(){
-    int i = 0,j = 0; bt[0] = -1;
+    int i = 0,j = -1; bt[0] = -1;
     while(i<P.size()){
         while(j>=0 && P[i]!=P[(j>=0?j:0)]) j = bt[j];
         i++;j++; bt[i] = j;
