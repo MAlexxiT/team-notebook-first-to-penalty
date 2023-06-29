92df75856b8bdabfb42c79c848c4d3b9fcfbdac7
MAlexxiT <mauricio230600@gmail.com>
2023-06-29 10:16:44 -0600

    Se cambio el comportamiento de busqueda para que ahora solo busque un indice cualquiera y no la primera y ultima ocurrencia, haciendolo mas confiable, tambien se recortaron como 30 lineas

--- Strings/suffixArray.cpp
+++ Strings/suffixArray.cpp
@@ -4,7 +4,6 @@
 #define ccd 12
 
 //metodos y structs auxiliares para el suffix array
-int techo(int n, int d){return (n+d-1)/d;}
 struct sufd{int id;long long int t;};
 int getndigit(long long int num, int d){
     long long int p = 1;while(d--) p*=10LL;
@@ -80,51 +79,32 @@ int recque(int l, int r, int sti, int stil, int stir){
     int stim = stil+stir; stim/=2;
     return min(recque(l,r,sti*2+1,stil,stim),recque(l,r,sti*2+2,stim+1,stir));
 }
-int getlce(int l, int r){return recque(l,r,0,0,stsize/2);}
-int sfor(int l, int r, int lcpl, int lcpr){
-    int lcpm = min(lcpl,lcpr); int m = (l+r)/2; int n = T.size();
-    int lce =  getlce(m,r);
-    if(l>r) return -1;
-    if(lcpm>lce && l!=r && (T[sa[m]+lcpm]!=P[lcpm])) return sfor(m+1,r,lcpm,lcpr);
-    if(lcpm<lce && l!=r && (T[sa[m]+lcpm]!=P[lcpm])) return sfor(l,m,lcpl,lcpm);
-    for(int i = lcpm; sa[m]+i<n && i<P.size();  i++){lcpm = i;if(T[sa[m]+i]!=P[i]) break;}
-    if(l==r && lcpm+1 != P.size()) return -1;
-    if(l==r && lcpm+1 == P.size()) return l;
-    if(lcpm == P.size()) return sfor(l,m,lcpl,lcpm);
-    if(lcpm+sa[m] == n) return sfor(m+1,r,lcpm,lcpr);
-    return (T[lcpm+sa[m]]<P[lcpm]? sfor(m+1,r,lcpm,lcpr):sfor(l,m,lcpl,lcpm));
+int getlce(int l, int r){
+    if(l==r) return T.size()-sa[l];
+    return recque(l+1,r,0,0,stsize/2);
 }
-int searchfo(){
-    int r = 0;
-    for(int i = 0; i<P.size() && i+sa[T.size()-1]<T.size(); i++){
+int buscarRec(int l, int r,int lcpl, int lcpr, bool izq){
+    int m = (l+r)/2; int n = T.size();
+    int lce = (izq?getlce(m+1,r):getlce(l,m));
+    int lcpm = min(lcpl,lcpr);
+    if(lcpm<lce && l!=r) return buscarRec(m+1,r,lcpm,lcpr,false);
+    if(lcpm>lce && l!=r) return buscarRec(l,m-1,lcpm,lcpr,true);
+    for(int i = lcpm; sa[m]+i<n && i<P.size(); i++){if(P[i]!=T[sa[m]+i]) break; lcpm++;}
+    if(lcpm == P.size()) return m;
+    if(l==r) return -1;
+    return (P[lcpm]>T[sa[m]+lcpm]?buscarRec(m+1,r,lcpm,lcpr,false):buscarRec(l,m,lcpm,lcpr,true));
+}
+int buscar(){
+    int l = 0; int n = T.size();
+    for(int i = 0; i<P.size() && i+sa[n-1]<n; i++){
         if(T[i+sa[T.size()-1]] != P[i]) break;
-        r++;
+        l++;
     }
-    return sfor(0,T.size()-1,0,r);
+    return buscarRec(0,n-1,0,l,false);
 }
-int slor(int l, int r, int lcpl, int lcpr){
-    int lcpm = min(lcpl,lcpr); int m = techo(l+r,2); int n = T.size();
-    int lce =  getlce(m,r);
-    if(l==r) return l;
-    if(l>r) return -1;
-    if(lcpm>lce && l!=r && (T[sa[m]+lcpm]!=P[lcpm])) return slor(m,r,lcpm,lcpr);
-    if(lcpm<lce && l!=r && (T[sa[m]+lcpm]!=P[lcpm])) return slor(l,m,lcpl,lcpm);
-    for(int i = lcpm; sa[m]+i<n && i<P.size();  i++){lcpm = i;if(T[sa[m]+i]!=P[i]) break;}
-    if(l==r && lcpm+1 != P.size()) return -1;
-    if(l==r && lcpm+1 == P.size()) return l;
-    if(lcpm == P.size() || lcpm+sa[m] == n) return slor(m,r,lcpm,lcpr);
-    
-    return (T[lcpm+sa[m]]<=P[lcpm]? slor(m,r,lcpm,lcpr):slor(l,m-1,lcpl,lcpm));
-}
-int searchlo(){
-    int r = 0;
-    for(int i = 0; i<P.size() && i+sa[T.size()-1]<T.size(); i++){
-        if(T[i+sa[T.size()-1]] != P[i]) break;
-        r++;
-    }
-    return slor(0,T.size()-1,0,r);
-}
-//CODIGO DE 130 LINEAS, TE HE FALLADO MarcosK
-//Uso: lee T, agregar signo dolar, llama makesa(); makelce(); lee P search last/first ocurr
+//CODIGO DE 100 LINEAS, TE HE FALLADO MarcosK
+//Uso: lee T, agregar signo dolar, llama makesa(); makelce(); lee P para despues buscar()
 //delete[] sa; delete[] lcest; cuando leas de nuevo T
-//O(|T|) preprocesamiento, O(|P|+log**2(|T|)) cada busqueda
+//O(|T| log(|T|)) preprocesamiento, O(|P|+log**2(|T|)) cada busqueda
+//Buscar devuelve un indice cualquiera de sa tal que el sufijo denotado tenga P como prefijo 
+//¿Se puede hacer mas corto?
