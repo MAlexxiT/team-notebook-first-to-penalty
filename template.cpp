62039317bc61abeae7ba85ba95d52676cf07bb91
MAlexxiT <mauricio230600@gmail.com>
2024-11-07 00:26:18 -0700

    Added a comment on template demonstrating the use of getline

--- template.cpp
+++ template.cpp
@@ -9,8 +9,9 @@ using namespace std;
 //#define RAYA ;
 //#define RAYAS ;
 void solve(){
-
-}
+	//use the following for strings with whitespaces in them
+	//getline(cin,str)
+}	
 //--------------------SOLBEGIN-----------------------------------
 int main() {
 	ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
